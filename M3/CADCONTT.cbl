15b96a25e0b252c3fb73147506002e817caad278
uelberbell <72271381+uelberbell@users.noreply.github.com>
2023-05-04 15:39:13 -0300

    Programa Cadastro
    Iniciando programa cadastro trabalhando com arquivos e layout

---
+++ M3/CADCONTT.cbl
@@ -0,0 +1,37 @@
+      ******************************************************************
+      * Author:UELBER PEREIRA DE JESUS
+      * Date:04/05/2023
+      * Purpose:CADASTRO DE CONTATOS
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. CADCONTT. *>BOA PRATICA, TAMANHO COM 8 CARACTERS.
+
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       SPECIAL-NAMES.
+           DECIMAL-POINT IS COMMA. *> PONTO SERÁ CONSEIDERADO VIRGULA.
+           INPUT-OUTPUT SECTION.
+           FILE-CONTROL.
+               SELECT CONTATOS ASSIGN TO
+               "C:\Users\escrtorio\Documents\COBOL\M3\CONTATOS.TXT"
+               ORGANISATION IS SEQUENTIAL
+               ACCESS MODE IS SEQUENTIAL
+               FILE STATUS IS WS-FS.
+
+
+       DATA DIVISION.
+       FILE SECTION.
+       FD CONTATOS
+           01 REG-CONTATOS.
+               03 CD-CONTATO       PIC 9(05).
+               03 NM-CONTATO       PIC X(15).
+
+       WORKING-STORAGE SECTION.
+       77 WS-FS        PIC 99.
+           88 FS-OK    VALUE 0.
+
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+
+            STOP RUN.
+       END PROGRAM CADCONTT. *>BOA PRATICA, TAMANHO COM 8 CARACTERS.
