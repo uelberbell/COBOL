2be2ac7b794af3b787dd60fb3e5ffca90bebec9c
uelberbell <72271381+uelberbell@users.noreply.github.com>
2023-05-30 18:00:26 -0300

    Caller program menu

---
+++ M3/MENUCNTT.cbl
@@ -0,0 +1,18 @@
+      ******************************************************************
+      * Author:Uelber Pereira de Jesus
+      * Date:30/05/2023
+      * Purpose:Chamar programas externos
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. MENUCNTT.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+
+
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "FUNCIONANDO MENU CHAMADOR"
+            STOP RUN.
+       END PROGRAM MENUCNTT.
