5753ada7e8fff99e503f68209a1bac8f79e6e492
uelberbell <uelberbell@gmail.com>
2023-08-15 06:58:30 -0300

    update programm FINAC

--- M4/FINANCIAMENTO/FINANC.cbl
+++ M4/FINANCIAMENTO/FINANC.cbl
@@ -3,6 +3,7 @@
       * Date:
       * Purpose:
       * Tectonics: ARRAY DINAMICO
+      * Update: Tratando de parcela menor que zero ou maior que 420.
       ******************************************************************
        IDENTIFICATION DIVISION.
        PROGRAM-ID. FINANC.
@@ -18,13 +19,13 @@
            03 WS-NOME      PIC A(18) VALUE SPACES.
            03 WS-NM-PRODUTO        PIC A(18) VALUE SPACES.
            03 WS-VALOR             PIC 9(06)V99.
-           03 WS-NUM-PARCELAS      PIC 99.
+           03 WS-NUM-PARCELAS      PIC 999.
            03 WS-PARCELAS       PIC $$.$$9,99 OCCURS 1 TO 420 TIMES
                                 DEPENDING ON WS-NUM-PARCELAS.
 
        01 WS-VARIAVEIS.
            03 WS-VL-PARCELAS       PIC 9(05)V99.
-           03 WS-IND               PIC 99.
+           03 WS-IND               PIC 999.
 
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
@@ -37,8 +38,15 @@
                ACCEPT WS-NM-PRODUTO.
                DISPLAY "INFORME O VALOR DO PRODUTO: "
                ACCEPT WS-VALOR.
+           P00PARC.
                DISPLAY "INFORME O NUMERO DE VEZES QUE DESEJA PORCELAR: "
                ACCEPT WS-NUM-PARCELAS
+
+                IF WS-NUM-PARCELAS = 0 OR > 420 THEN
+                DISPLAY "VALOR DE PARCELA PRECISA SER MAIOR QUE ZERO(0)"
+                        "OU MENOR QUE 420"
+                 PERFORM P00PARC
+                .
 
                COMPUTE WS-VL-PARCELAS = WS-VALOR / WS-NUM-PARCELAS
 
@@ -51,7 +59,7 @@
                                WS-IND > WS-NUM-PARCELAS
                    DISPLAY "PARCELA " WS-IND ": " WS-PARCELAS(WS-IND)
 
-               END-PERFORM
-
+               END-PERFORM.
+           P00FIM.
             STOP RUN.
        END PROGRAM FINANC.
